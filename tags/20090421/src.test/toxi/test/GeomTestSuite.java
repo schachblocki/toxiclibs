b4792c6252f8b677f2378228fdf089c59c6a8cf0
toxmeister <toxmeister@a28ade32-462b-0410-8b6e-5d8b477a1069>
2009-05-01 02:55:08 +0000

    state of the 2009-04-21 releases: core-0013, colorutils-0002, verletphysics-0004, volumeutils-0001

---
+++ tags/20090421/src.test/toxi/test/GeomTestSuite.java
@@ -0,0 +1,80 @@
+package toxi.test;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import toxi.geom.AABB;
+import toxi.geom.PointOctree;
+import toxi.geom.Ray3D;
+import toxi.geom.Sphere;
+import toxi.geom.SphereIntersectorReflector;
+import toxi.geom.Vec3D;
+import toxi.math.MathUtils;
+
+public class GeomTestSuite extends TestCase {
+
+	public void testAABB2AABB() {
+		AABB box = new AABB(new Vec3D(100, 0, 0), new Vec3D(20, 20, 20));
+		AABB b2 = new AABB(new Vec3D(100, 30.1f, 0), new Vec3D(10, 10, 10));
+		assertEquals(box.intersectsBox(b2), false);
+	}
+
+	public void testAABBSphere() {
+		AABB box = new AABB(new Vec3D(100, 0, 0), new Vec3D(20, 20, 20));
+		Sphere s = new Sphere(new Vec3D(100, 0, 0), 50);
+		assertEquals(box.intersectsSphere(s), true);
+	}
+
+	public void testClosestPoint() {
+		Vec3D a = new Vec3D();
+		Vec3D b = new Vec3D(100, 0, 0);
+		Vec3D c = new Vec3D(50, 50, 0);
+		Vec3D isec = c.closestPointOnLine(a, b);
+		assertEquals(MathUtils.abs(isec.x - c.x) < 0.5, true);
+		c = new Vec3D(-50, -50, 0);
+		isec = c.closestPointOnLine(a, b);
+		assertEquals(isec.equals(a), true);
+	}
+
+	public void testIsInAABB() {
+		AABB box = new AABB(new Vec3D(100, 0, 0), new Vec3D(20, 20, 20));
+		Vec3D p = new Vec3D(80, -19.99f, 0);
+		assertEquals(p.isInAABB(box), true);
+		assertEquals(new Vec3D(120.01f, 19.99f, 0).isInAABB(box), false);
+	}
+
+	public void testIsInSphere() {
+		Vec3D p = new Vec3D(0, -10, 0);
+		Sphere s = new Sphere(new Vec3D(), 10);
+		assertEquals(s.containsPoint(p), true);
+		p.set(0, 10.1f, 0);
+		assertEquals(s.containsPoint(p), false);
+	}
+
+	public void testOctree() {
+		PointOctree t = new PointOctree(new Vec3D(), 100);
+		assertEquals(t.addPoint(new Vec3D(0, 0, 0)), true);
+		assertEquals(t.addPoint(new Vec3D(0, 100, 0)), true);
+		assertEquals(t.addPoint(new Vec3D(101, 0, 0)), false);
+		ArrayList points = t.getPointsWithinSphere(new Sphere(new Vec3D(50, 0,
+				0), 50));
+		assertEquals(points.size() == 1, true);
+		points = t.getPointsWithinBox(new AABB(new Vec3D(50, 50, 50),
+				new Vec3D(50, 50, 50)));
+		assertEquals(points.size() == 2, true);
+	}
+
+	public void testReflectRay() {
+		SphereIntersectorReflector si = new SphereIntersectorReflector(
+				new Vec3D(0, 0, 0), 10);
+		Ray3D r = si.reflectRay(new Ray3D(new Vec3D(0, 100, 0), new Vec3D(-1,
+				-1, 0)));
+		assertEquals(MathUtils.abs(r.getDirection().y - 1) < 0.002, true);
+	}
+
+	/*
+	 * public void testSlerp() { Quaternion a=new Quaternion(Vec3D.X_AXIS,0);
+	 * Quaternion b=new Quaternion(Vec3D.Y_AXIS,0); a.interpolateTo(b, 0.5);
+	 * System.out.println(a); }
+	 */
+}
